088a3cda5e006c80578c7a7477a680daa7c9b1a6
Cesar Felipe Reis Alberto <metaces@gmail.com>
2019-05-23 12:59:29 -0300

    utilizando listCreatApiView em Task

--- django/todoapi/views.py
+++ django/todoapi/views.py
@@ -1,12 +1,32 @@
-from rest_framework import generics
-
 from .models import Task
 from .serializers import TaskSerializer
 
-class TaskList(generics.ListAPIView):
+from rest_framework import generics
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+
+class TaskList(generics.ListCreateAPIView):
+    # """
+    # VIEW ALL TASKS
+    # """
+    # def get(self, request, format=None):
+    #     task = Task.objects.all()
+    #     serializer = TaskSerializer(task, many=True)
+    #     return Response(serializer.data)
+
+    # def post(self, request, format=None):
+    #     """
+    #     CREATE A TASK
+    #     """
+    #     serializer = TaskSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     """
-    VIEW ALL TASKS
+    LIST AND CREATE TASKS
     """
-    task = Task.objects.all()
-    serializer = TaskSerializer
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
     
